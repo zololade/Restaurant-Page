f660c0ea4397bc43db5feb515c23e8dc7f2adb3a
Ololade <73138104+zololade@users.noreply.github.com>
2026-02-15 10:04:05 +0100

    feat: add the needed pacakages

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+// webpack.config.js
+import path from "node:path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+export default {
+ mode: "development",
+ entry: "./src/index.js",
+ output: {
+  filename: "main.js",
+  path: path.resolve(import.meta.dirname, "dist"),
+  clean: true,
+ },
+ devtool: "eval-source-map",
+ devServer: {
+  watchFiles: ["./src/template.html"],
+ },
+ plugins: [
+  new HtmlWebpackPlugin({
+   template: "./src/template.html",
+  }),
+ ],
+ module: {
+  rules: [
+   {
+    test: /\.css$/i,
+    use: ["style-loader", "css-loader"],
+   },
+   {
+    test: /\.html$/i,
+    use: ["html-loader"],
+   },
+   {
+    test: /\.(png|svg|jpg|jpeg|gif)$/i,
+    type: "asset/resource",
+   },
+  ],
+ },
+};
