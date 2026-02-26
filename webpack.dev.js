fc184003322570a2edf93dd3a25323241cb8d63b
Ololade <73138104+zololade@users.noreply.github.com>
2026-02-26 09:42:51 +0100

    fix: rewrite webpack configuration

---
+++ webpack.dev.js
@@ -0,0 +1,16 @@
+import { merge } from "webpack-merge";
+import common from "./webpack.common.js";
+import path from "node:path";
+
+const __dirname = import.meta.dirname;
+
+export default merge(common, {
+ mode: "development",
+ devtool: "eval-source-map",
+ devServer: {
+  watchFiles: ["./src/template.html"],
+  static: path.join(__dirname, "dist"),
+  compress: true,
+  port: 4000,
+ },
+});
